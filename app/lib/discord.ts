7524d1ae15c7e7d5ef0e5d7ff57af31363996c4b
sehoon-git <aabb0125@naver.com>
2026-08-04 20:37:25 +0900

    Use guild bot invite link

--- app/lib/discord.ts
+++ app/lib/discord.ts
@@ -1,5 +1,5 @@
 const DEFAULT_BOT_INVITE_URL =
-  "https://discord.com/oauth2/authorize?client_id=1533876491312697344&scope=bot+applications.commands";
+  "https://discord.com/oauth2/authorize?client_id=1533876491312697344&permissions=2150714368&integration_type=0&scope=bot+applications.commands";
 
 export function getDiscordBotInviteUrl() {
   return process.env.DISCORD_BOT_INVITE_URL ?? DEFAULT_BOT_INVITE_URL;
